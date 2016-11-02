fc02e42cab367a631efab916c0fbe6c26a0a9e32
sharmava <sharmava@indiana.edu>
2016-11-02 01:05:22 -0500

    Adding implemetation for topological ordering

---
+++ src/advancedAlgo/topologicalOrdering/TopologicalOrdering.java
@@ -0,0 +1,28 @@
+package advancedAlgo.topologicalOrdering;
+
+import java.util.Stack;
+
+public class TopologicalOrdering {
+	private Stack<Vertex> stack;
+ 
+	public TopologicalOrdering(){
+		this.stack = new Stack<Vertex>();
+	}
+	
+	public void dfs(Vertex v){
+    	v.setVisited(true);
+    	
+    	for(Vertex x : v.getAdjacenciesList()){
+    		if(!x.isVisited()){
+    			dfs(x);
+    		}
+    		
+    	}
+    	this.stack.push(v);
+    }
+	
+	public Stack<Vertex> getStack(){
+		return this.stack;
+	}
+}
+ 
---
+++ src/advancedAlgo/topologicalOrdering/Vertex.java
@@ -0,0 +1,51 @@
+package advancedAlgo.topologicalOrdering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex {
+    private String data;
+    private boolean visited;
+    private boolean beingVisited;
+    private List<Vertex> adjacenciesList;
+    
+    public Vertex(String data){
+    	this.data = data;
+    	this.adjacenciesList = new ArrayList<Vertex>();
+    }
+    
+    public void addVertex(Vertex v){
+    	this.adjacenciesList.add(v);
+    }
+    
+    @Override
+    public String toString(){
+    	return this.data;
+    }
+    
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public boolean isVisited() {
+		return visited;
+	}
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+	public boolean isBeingVisited() {
+		return beingVisited;
+	}
+	public void setBeingVisited(boolean beingVisited) {
+		this.beingVisited = beingVisited;
+	}
+	public List<Vertex> getAdjacenciesList() {
+		return adjacenciesList;
+	}
+	public void setAdjacenciesList(List<Vertex> adjacenciesList) {
+		this.adjacenciesList = adjacenciesList;
+	}
+    
+}
